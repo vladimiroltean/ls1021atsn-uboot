u-boot/u-boot

commit 5c33c9fdbb3f074676466b18c95dd64e8e6cf6d7

fdt: Add a function to get the alias sequence of a node
Aliases are used to provide U-Boot's numbering of devices, such as:

aliases {
	spi0 = "/spi@12330000";
}

spi@12330000 {
	...
}

This tells us that the SPI controller at 12330000 is considered to be the
first SPI controller (SPI 0). So we have a numbering for the SPI node.

Add a function that returns the numbering for a node assume that it exists
in the list of aliases.

Signed-off-by: Simon Glass <sjg@chromium.org>

lib/fdtdec.c
@@ -5,9 +5,11 @@
 
 #ifndef USE_HOSTCC
 #include <common.h>
+#include <errno.h>
 #include <serial.h>
 #include <libfdt.h>
 #include <fdtdec.h>
+#include <linux/ctype.h>
 
 #include <asm/gpio.h>
 
@@ -317,6 +319,50 @@ int fdtdec_add_aliases_for_id(const void *blob, const char *name,
 	}
 
 	return num_found;
+}
+
+int fdtdec_get_alias_seq(const void *blob, const char *base, int offset,
+			 int *seqp)
+{
+	int base_len = strlen(base);
+	const char *find_name;
+	int find_namelen;
+	int prop_offset;
+	int aliases;
+
+	find_name = fdt_get_name(blob, offset, &find_namelen);
+	debug("Looking for '%s' at %d, name %s\n", base, offset, find_name);
+
+	aliases = fdt_path_offset(blob, "/aliases");
+	for (prop_offset = fdt_first_property_offset(blob, aliases);
+	     prop_offset > 0;
+	     prop_offset = fdt_next_property_offset(blob, prop_offset)) {
+		const char *prop;
+		const char *name;
+		const char *slash;
+		const char *p;
+		int len;
+
+		prop = fdt_getprop_by_offset(blob, prop_offset, &name, &len);
+		debug("   - %s, %s\n", name, prop);
+		if (len < find_namelen || *prop != '/' || prop[len - 1] ||
+		    strncmp(name, base, base_len))
+			continue;
+
+		slash = strrchr(prop, '/');
+		if (strcmp(slash + 1, find_name))
+			continue;
+		for (p = name; *p; p++) {
+			if (isdigit(*p)) {
+				*seqp = simple_strtoul(p, NULL, 10);
+				debug("Found seq %d\n", *seqp);
+				return 0;
+			}
+		}
+	}
+
+	debug("Not found\n");
+	return -ENOENT;
 }
 
 int fdtdec_check_fdt(void)

include/fdtdec.h
@@ -345,6 +345,24 @@ int fdtdec_find_aliases_for_id(const void *blob, const char *name,
 int fdtdec_add_aliases_for_id(const void *blob, const char *name,
 			enum fdt_compat_id id, int *node_list, int maxcount);
 
+/**
+ * Get the alias sequence number of a node
+ *
+ * This works out whether a node is pointed to by an alias, and if so, the
+ * sequence number of that alias. Aliases are of the form <base><num> where
+ * <num> is the sequence number. For example spi2 would be sequence number
+ * 2.
+ *
+ * @param blob		Device tree blob (if NULL, then error is returned)
+ * @param base		Base name for alias (before the underscore)
+ * @param node		Node to look up
+ * @param seqp		This is set to the sequence number if one is found,
+ *			but otherwise the value is left alone
+ * @return 0 if a sequence was found, -ve if not
+ */
+int fdtdec_get_alias_seq(const void *blob, const char *base, int node,
+			 int *seqp);
+
 /*
  * Get the name for a compatible ID
  *

vladimiroltean/ls1021atsn-uboot

commit 8115c93217fae3840f1448dfd48ae68a73475e7e

fdtdec: use full path in fdtdec_get_alias_seq comparison
Currently fdtdec_get_alias_seq() calls fdt_get_name() which returns only
the name of the leaf node. So it needs to also trim the path of the
alias to the leaf name only, leading to imperfect matches.

This means that the following aliases:

    /aliases {
        eth0 = "/dspi@2120000/ethernet-switch@0/ports/port@0";
        eth1 = "/pcie@1f0000000/pci@0,5/ports/port@0";
    };

will make fdtdec_get_alias_seq to return a seq of zero for both aliases,
as the match will only take place on the "port@0" portion.

Fix this by calling fdt_get_path and comparing the full path of the
alias.

Fixes: 5c33c9fdbb3f ("fdt: Add a function to get the alias sequence of a node")
Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>

lib/fdtdec.c
@@ -455,13 +455,14 @@ int fdtdec_get_alias_seq(const void *blob, const char *base, int offset,
 			 int *seqp)
 {
 	int base_len = strlen(base);
-	const char *find_name;
-	int find_namelen;
 	int prop_offset;
+	char path[64];
+	int path_len;
 	int aliases;
 
-	find_name = fdt_get_name(blob, offset, &find_namelen);
-	debug("Looking for '%s' at %d, name %s\n", base, offset, find_name);
+	fdt_get_path(blob, offset, path, sizeof(path));
+	path_len = strlen(path);
+	debug("Looking for '%s' at %d, path %s\n", base, offset, path);
 
 	aliases = fdt_path_offset(blob, "/aliases");
 	for (prop_offset = fdt_first_property_offset(blob, aliases);
@@ -469,17 +470,15 @@ int fdtdec_get_alias_seq(const void *blob, const char *base, int offset,
 	     prop_offset = fdt_next_property_offset(blob, prop_offset)) {
 		const char *prop;
 		const char *name;
-		const char *slash;
 		int len, val;
 
 		prop = fdt_getprop_by_offset(blob, prop_offset, &name, &len);
 		debug("   - %s, %s\n", name, prop);
-		if (len < find_namelen || *prop != '/' || prop[len - 1] ||
+		if (len < path_len || *prop != '/' || prop[len - 1] ||
 		    strncmp(name, base, base_len))
 			continue;
 
-		slash = strrchr(prop, '/');
-		if (strcmp(slash + 1, find_name))
+		if (strcmp(prop, path))
 			continue;
 		val = trailing_strtol(name);
 		if (val != -1) {